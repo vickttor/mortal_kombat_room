fc05b807e10bec073c2eaa8f68154fcb4a27a4c9
Victor Hugo da Silva <victor.silvaPC@hotmail.com>
2022-01-28 23:58:41 -0300

    environment variable fixed

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -4,7 +4,7 @@ import appConfig from '../config.json';
 // SUPABASE
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
