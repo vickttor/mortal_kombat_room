fa9dcdb761c8242e2b15ff9cc881b7b15054ef2e
Victor Hugo da Silva <victor.silvaPC@hotmail.com>
2022-01-30 12:11:42 -0300

    environment variables fixed

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -15,8 +15,8 @@ import {ButtonSendSticker} from "../src/components/ButtonSendSticker";
 import { createClient } from "@supabase/supabase-js";
 
 // Getting environments variables
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // Creating connection with supabase
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
