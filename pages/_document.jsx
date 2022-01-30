9be46c6b587b05048e9b81ba18e98ace3c3a6e0a
Victor Hugo da Silva <victor.silvaPC@hotmail.com>
2022-01-30 15:58:01 -0300

    changed fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,8 +6,8 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                    <link href="https://fonts.googleapis.com/css2?family=Acme&family=Nunito+Sans:wght@200;400;700;800&display=swap" rel="stylesheet"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet"/>
                     <link rel="shortcut icon" href="/assets/tabIcon.png" type="image/x-icon" />
                 </Head>
                 <body>
