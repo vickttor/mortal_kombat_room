4e808f300b1f8e6ab4035af8a2833fc47f6a9268
Victor Hugo da Silva <victor.silvaPC@hotmail.com>
2022-01-30 15:59:57 -0300

    fetch request updated v1.5

--- src/api/github.js
+++ src/api/github.js
@@ -1,11 +1,12 @@
 export default function RequestGithubAPI(username){
-    return fetch(`https://api.github.com/users/${username || 'skynexui'}`)
+    return fetch(`https://api.github.com/users/${username}`)
     .then((response)=>response.json())
     .then((data)=>{
         return {
             name:data.name,
             location:data.location,
             repositories:data.public_repos,
+            login:data.login
         }
     })
 }
